b68095b47192d98400fff0d22c12838418638f4d
Your Name <you@example.com>
2026-04-12 18:40:40 -0600

    Limpieza: Eliminando archivos ajenos al proyecto Android

--- dorama-loader.js
+++
@@ -1,8 +0,0 @@
-function cargarDoramaExpress() {
-  const script = document.createElement('script');
-  script.src = 'https://raw.githubusercontent.com/municipalidad1998/peliculas2025/main/DoramaExpress.js';
-  script.async = true;
-  document.head.appendChild(script);
-}
-
-cargarDoramaExpress();
